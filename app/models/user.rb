4e328a5272cdd22794a8b32237961463931ca588
Stu <stumitchell@irsss.ca>
2022-07-28 00:28:59 +0000

    W4D2, adding signup and database functionality

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,7 @@ class User < ActiveRecord::Base
     has_many :finstagram_posts
     has_many :likes
     has_many :comments
-    
+   
+    validates :email, :username, uniqueness: true
+    validates :email, :avatar_url, :username, :password, presence: true
 end
